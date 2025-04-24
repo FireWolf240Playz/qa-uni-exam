5ac7d79ec88d5ae5319b7198bb43f13c99b220a0
FireWolf240Playz <a.yordanow67@gmail.com>
2025-04-24 18:44:54 +0300

    removed unused imports

--- upload_test.py
+++ upload_test.py
@@ -1,9 +1,7 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-import time
 
 
 class InputFieldTest(unittest.TestCase):
